adfc1d304ccee858007cbc4620aa24bed501a911
SpoonKa <spoon1998spoon@gmail.com>
2025-05-13 20:56:01 +0200

    ChekPoint(from PLayer)

--- Game_Car-2/Assets/Script/GamePlayControl/Raise/Chekpoint.cs
+++ Game_Car-2/Assets/Script/GamePlayControl/Raise/Chekpoint.cs
@@ -5,12 +5,21 @@ public class Chekpoint : MonoBehaviour
 {
     [SerializeField] private string _targetTags = "Player";
     
-    private void OnTriggerEnter(Collider other)
+    public void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject == GameObject.FindGameObjectWithTag(_targetTags))
+       if(other.gameObject == GameObject.FindGameObjectWithTag(_targetTags))
         {
+            RaiseChekpoint manager = FindFirstObjectByType<RaiseChekpoint>();
+            if (manager != null)
+            {
+                manager.UpdateCheckpoint(transform);
+            }
+
             gameObject.SetActive(false);
+       
         }
+
+
     }
   
 }
--- Game_Car-2/Assets/Script/GamePlayControl/Raise/RaiseChekpoint.cs
+++ Game_Car-2/Assets/Script/GamePlayControl/Raise/RaiseChekpoint.cs
@@ -1,22 +1,51 @@
-using UnityEngine;
+
+using System;
 using System.Collections.Generic;
+using UnityEngine;
+
 public class RaiseChekpoint : MonoBehaviour
 {
     [SerializeField] private Transform _firstPoint;
     [SerializeField] private Transform _Finish;
 
-    [SerializeField] private List<Transform> _chekpoint;
-
+    private List<Transform> _chekpoints = new List<Transform>();
+    private Transform _car;
     public Transform CurentPoint { get;  private set; }
+    public event Action<int,int> OnChekPointChenge;
 
     public void Awake()
     {
-        CurentPoint = _firstPoint; 
-        
-    }
-    private void SetCurentPoint()
-    {
-       
+        _chekpoints.Clear();
+
+        CurentPoint = _firstPoint.transform;
+        var chArray = GameObject.FindGameObjectsWithTag("Chekpoint");
+
+        Array.Sort(chArray, (a, b) => string.Compare(a.name, b.name));
+
+        _chekpoints.Add(_firstPoint);
+        foreach (var chek in chArray)
+        {
+            var transform = chek.GetComponent<Transform>();
+            _chekpoints.Add(transform);
+        }
+        _chekpoints.Add(_Finish);
+
+
+        var car = GameObject.FindGameObjectWithTag("Player");
+        _car = car.transform;
+        OnChekPointChenge?.Invoke(0, _chekpoints.Count - 1);
     }
 
+    public void UpdateCheckpoint(Transform nextPoint)
+    {
+        var index = _chekpoints.IndexOf(nextPoint);
+
+        if (index!= -1)
+        {
+            CurentPoint = nextPoint;
+            var rem = _chekpoints.Count - 1 - index;
+            Debug.Log($"Checkpoint updated to: {CurentPoint.name}");
+            OnChekPointChenge?.Invoke(index, rem);
+        }
+    }
 }
